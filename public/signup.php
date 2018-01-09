a7013882826aa0ea33e17243a8b9e1d175dc0389
EmixMaxime <emixmaxime@outlook.fr>
2018-01-09 21:59:27 +0100

    (add) handle POST request and get form expected params

--- public/signup.php
+++ public/signup.php
@@ -1,3 +1,11 @@
 <?php
 
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $firstName = $_POST['first-name'] ?? null;
+    $lastName = $_POST['last-name'] ?? null;
+    $email = $_POST['email'] ?? null;
+    $password = $_POST['password'] ?? null;
+    $repeatPassword = $_POST['repeat-password'] ?? null;
+}
+
 require '../views/signup.view.php';
