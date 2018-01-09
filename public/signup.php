4dfdd1d8298d31ccf255a39aa0bb62e172b27260
EmixMaxime <emixmaxime@outlook.fr>
2018-01-09 22:24:48 +0100

    (add) basis form validation

--- public/signup.php
+++ public/signup.php
@@ -10,6 +10,33 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     var_dump($params); // ['first-name' => 'Maxime', 'last-name' => null, ...]
+
+    /**
+     * Validation des entrées du formulaire
+     */
+
+    if (is_null($params['first-name']) || empty($params['first-name'])) {
+        echo 'Veuillez remplir first-name';
+    }
+
+    if (is_null($params['last-name']) || empty($params['last-name'])) {
+        echo 'Veuillez remplir last-name';
+    }
+
+    if (is_null($params['email']) || empty($params['email'])) {
+        echo 'Veuillez remplir email';
+    }
+
+    if (is_null($params['password']) || empty($params['password'])) {
+        echo 'Veuillez remplir password';
+    }
+
+    if (is_null($params['repeat-password']) || empty($params['repeat-password'])) {
+        echo 'Veuillez remplir repeat-password';
+    } else if ($params['repeat-password'] !== $params['password'] && (!is_null($params['password']) && !empty($params['password']))) {
+        echo 'Vos mots de passe ne correspondent pas !';
+    }
+
 }
 
 require '../views/signup.view.php';
