b24f1198f4a25d5bf8f02d2aff13abb165f2f7f5
EmixMaxime <emixmaxime@outlook.fr>
2018-01-09 22:06:55 +0100

    (improve) get post params into assoc array

--- public/signup.php
+++ public/signup.php
@@ -4,12 +4,12 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $expectedFields = ['first-name', 'last-name', 'email', 'password', 'repeat-password'];
 
     $params = [];
-    foreach($expectedFields as $field) {
-        $param = $_POST[$field] ?? null;
-        array_push($params, $param);
+    foreach($expectedFields as $fieldName) {
+        $value = $_POST[$fieldName] ?? null;
+        $params[$fieldName] = $value;
     }
 
-    var_dump($params); // -> ['Maxime', 'Moreau', null, 'bonjour', 'bonjour']
+    var_dump($params); // ['first-name' => 'Maxime', 'last-name' => null, ...]
 }
 
 require '../views/signup.view.php';
