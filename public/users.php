a6c1005b26f478034ed1a7b911fca1f6b6bc8b37
EmixMaxime <emixmaxime@outlook.fr>
2018-01-10 21:56:40 +0100

    (add :tada:) first database query with PDO !!

--- public/users.php
+++ public/users.php
@@ -1,6 +1,9 @@
 <?php
-
 require('../src/view.php');
+require('../src/database.php');
+
+$users = $pdo->query('SELECT * FROM users')->fetchAll(PDO::FETCH_ASSOC);
+var_dump($users);
+die;
+
 render('users');
-
-
---
+++ src/database.php
@@ -0,0 +1,4 @@
+<?php
+
+$dsn = 'pgsql:host=localhost user=mx dbname=beginners_member_php password=coucou';
+$pdo = new PDO($dsn);
