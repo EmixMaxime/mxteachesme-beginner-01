96cf8afbd9bc2a18872e86c299b2910fa794a7f2
EmixMaxime <emixmaxime@outlook.fr>
2018-01-10 21:55:43 +0100

    (add) users.data.sql

---
+++ sql/users.data.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (email, first_name, last_name, password) VALUES (
+    'something@gmail.com', 'Maxime', 'Moreau', 'something'
+), (
+    'somethingelse@gmail.com', 'Victor', 'Andrusiak', 'somethingelse'
+), (
+    'hello-world@mxteaches.me', 'Hello', 'world', 'some-secure-password'
+);
