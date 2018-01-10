4ca03d7bde74c2cee774dbad70350ba3e79f0ff6
EmixMaxime <emixmaxime@outlook.fr>
2018-01-10 22:59:59 +0100

    (add :tada:) DELETE USER ACTION !!

--- public/users.php
+++ public/users.php
@@ -1,8 +1,30 @@
 <?php
+
 require('../src/view.php');
 require('../src/session.php');
 require('../src/database.php');
+require('../src/http.php');
 
 $users = $pdo->query('SELECT * FROM users')->fetchAll(PDO::FETCH_ASSOC);
+
+$action = $_GET['action'] ?? null;
+
+if (!is_null($action)) {
+    $id = $_GET['id'] ?? null;
+
+    if (is_null($id)) {
+        addFlashMessage('warning', "Veuillez ne pas modifier l'url.");
+        redirect('users.php');
+    }
+
+    $c = $pdo->exec("DELETE FROM users WHERE id = $id");
+    if ($c === 0) {
+        addFlashMessage('warning', "Une erreur s'est produite lors de la suppression du compte.");
+        redirect('users.php');
+    }
+
+    addFlashMessage('information', "Le compte portant l'identifiant $id a bien été supprimé.");
+    redirect('users.php');
+}
 
 render('users');
