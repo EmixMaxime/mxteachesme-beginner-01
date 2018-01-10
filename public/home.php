5bbe0f6c67dd1db08c5721b0ed879b11c5ef972b
EmixMaxime <emixmaxime@outlook.fr>
2018-01-10 14:04:59 +0100

    (improve) extract render business from home.php to view.php

--- public/home.php
+++ public/home.php
@@ -1,15 +1,5 @@
 <?php
 
-require('../views/home.view.php');
+require('../src/view.php');
 
-if (function_exists('extends_layout')) {
-    ob_start();
-    extends_layout();
-    block_body();
-    $block = ob_get_clean();
-
-    require('../views/layout.view.php');
-
-    $content = yield_body($block);
-    echo $content;
-}
+render('home');
---
+++ src/view.php
@@ -0,0 +1,18 @@
+<?php
+
+function render(String $templateName): void
+{
+    require("../views/$templateName.view.php");
+
+    if (function_exists('extends_layout')) {
+        ob_start();
+        extends_layout();
+        block_body();
+        $block = ob_get_clean();
+
+        require('../views/layout.view.php');
+
+        $content = yield_body($block);
+        echo $content;
+    }
+}
