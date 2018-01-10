c06c342ee3e1d5ec785cedd0bcedc01dc915b300
EmixMaxime <emixmaxime@outlook.fr>
2018-01-10 13:28:48 +0100

    (add) flash message

---
+++ src/session.php
@@ -0,0 +1,26 @@
+<?php
+
+
+session_start();
+
+function addFlashMessage(String $key, String $message): void
+{
+    if (!isset($_SESSION['flash'])) {
+      $_SESSION['flash'] = [];
+    }
+
+    $_SESSION['flash'][$key] = $message;
+}
+
+function getFlashMessage(String $key): ?String
+{
+    if (isset($_SESSION['flash'])) {
+        $info = $_SESSION['flash'][$key] ?? null;
+        if (!is_null($info)) {
+            unset($_SESSION['flash'][$key]);
+            return $info;
+        }
+    }
+
+    return null;
+}
