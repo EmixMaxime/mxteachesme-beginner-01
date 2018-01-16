50b2166451e3a88fc056c9cfb2eaa6ec1c003712
EmixMaxime <emixmaxime@outlook.fr>
2018-01-16 15:01:11 +0100

    (improve) extract alerts

--- views/users.view.php
+++ views/users.view.php
@@ -13,35 +13,8 @@ function extends_layout() {
             <div class="w-full mb-6 lg:mb-0 lg:w-1/2 px-4 flex flex-col mx-auto">
 
                 <?php
-                $information = getFlashMessage('information');
-                $warning = getFlashMessage('warning');
-
-                if (!is_null($information)) {
-                ?>
-                    <div class="bg-teal-lightest border-t-4 border-teal rounded-b text-teal-darkest px-4 py-3 shadow-md mb-8" role="alert">
-                        <div class="flex">
-                            <div class="py-1"><svg class="fill-current h-6 w-6 text-teal mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
-                            <div>
-                                <p class="font-bold"><?= $information ?></p>
-                            </div>
-                        </div>
-                    </div>
-
-                <?php
-                }
-
-                if (!is_null($warning)) {
-                    ?>
-                    <div class="bg-orange-lightest border-t-4 border-orange rounded-b text-teal-darkest px-4 py-3 shadow-md mb-8" role="alert">
-                        <div class="flex">
-                            <div>
-                                <p class="font-bold"><?= $warning ?></p>
-                            </div>
-                        </div>
-                    </div>
-                <?php
-                }
-                ?>
+					require('parts/alerts.view.php');
+				?>
 
                 <div class="flex-grow flex flex-col bg-white border-t border-b sm:rounded sm:border shadow overflow-hidden">
 
