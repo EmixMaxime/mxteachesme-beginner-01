2f217bcd330aaabcd0ef5d8f095a13f3ba63f030
EmixMaxime <emixmaxime@outlook.fr>
2018-01-16 15:06:37 +0100

    (improve) database connexion

--- src/database.php
+++ src/database.php
@@ -2,3 +2,10 @@
 
 $dsn = 'pgsql:host=localhost user=mx dbname=beginners_member_php password=coucou';
 $pdo = new PDO($dsn);
+
+// http://php.net/manual/fr/pdo.error-handling.php
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+// http://php.net/manual/fr/pdo.exec.php
